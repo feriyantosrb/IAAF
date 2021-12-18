fcd29410b27e1dc99be7033b041a0ad4145f2999
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2021-12-18 23:06:41 +0700

    Update rekap.py

--- rekap.py
+++ rekap.py
@@ -26,6 +26,7 @@ st.title('DONASI IAAF')
 st.header('Okeee')
 st.dataframe(df)
 
+
 #Konfigurasi Pengaturan
 config = st.sidebar.radio("Pilih bagian yang ingin Anda ketahui", ('Deskripsi', 'Rekapitulasi', 'Kontak Kami'))
 #if......
@@ -47,7 +48,8 @@ if pilih == Jenis_unik [0]:
 else :
     st.write(data[1:])
 
-#upper right col
+
+#upper middle col
 #membuat nama unik
 nama_unik=list(df['Nama'].unique()) #nama unik
 jumlah_nominal = []
@@ -57,14 +59,91 @@ for nama in nama_unik:
 dic_nama_jumnom={'Nama':nama_unik,'Jumlah Donasi':jumlah_nominal}
 nama_jumnom = pd.DataFrame(dic_nama_jumnom)
 #print(nama_jumnom)
-choose = st.selectbox('pilih nama donatur',nama_unik)
-while i < len(choose) :
-    choose = choose[i]
-    nama_jumnom = nama_jumnom[i:i+1]
-    print(nama_jumnom)
-    i=i+1
+choose = st.selectbox('pilih nama donatur',nama_unik) # nama_unik[0],....
+if choose == nama_unik[0]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[1]:
+    st.write(nama_jumnom[1:2])
+if choose == nama_unik[2]:
+    st.write(nama_jumnom[2:3])
+if choose == nama_unik[3]:
+    st.write(nama_jumnom[3:4])
+if choose == nama_unik[4]:
+    st.write(nama_jumnom[4:5])
+if choose == nama_unik[5]:
+    st.write(nama_jumnom[5:6])
+if choose == nama_unik[6]:
+    st.write(nama_jumnom[6:7])
+if choose == nama_unik[7]:
+    st.write(nama_jumnom[7:8])
+if choose == nama_unik[8]:
+    st.write(nama_jumnom[8:9])
+if choose == nama_unik[9]:
+    st.write(nama_jumnom[9:10])
+if choose == nama_unik[10]:
+    st.write(nama_jumnom[10:11])
+if choose == nama_unik[11]:
+    st.write(nama_jumnom[11:12])
+if choose == nama_unik[12]:
+    st.write(nama_jumnom[12:13])
+if choose == nama_unik[13]:
+    st.write(nama_jumnom[13:14])
+if choose == nama_unik[14]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[15]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[16]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[17]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[18]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[19]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[20]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[21]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[22]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[23]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[24]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[25]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[26]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[27]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[28]:
+    st.write(nama_jumnom[0:1])
+if choose == nama_unik[29]:
+    st.write(nama_jumnom[0:1])
 
 
+#lower left col
+#tujuan unik
+tujuan_unik = list(df['Tujuan'].unique())
+donasi_tujuan = []
+for tujuan in tujuan_unik:
+    sub_tujuan = df[df['Tujuan']==tujuan]['Nominal'].astype(int)
+    donasi_tujuan.append(sub_tujuan.sum())
+dic_donasi_tujuan={'Tujuan':tujuan_unik,'Jumlah donasi':donasi_tujuan}
+don_tu = pd.DataFrame(dic_donasi_tujuan)
+pil_dontu = st.selectbox('pilih alamat donasi tujuan',donasi_tujuan)
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[0:1])
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[1:2])
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[2:3])
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[3:4])
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[4:5])
+if pil_dontu == donasi_tujuan[0]:
+    st.write(don_tu[5:6])
 
 #bawah
 #dic bulan unik, jumlah donasi (Donatur Tetap)
@@ -74,4 +153,15 @@ for bulan in bulan_unik:
     sub_bulan = df[df['Bulan']==bulan]['Nominal'].astype(int)
     donasi_bulan.append(sub_bulan.sum())
 dic_donasi_bulan={'Bulan':bulan_unik,'Jumlah Donasi':donasi_bulan}
-st.dataframe(dic_donasi_bulan)
+don_bul = pd.DataFrame(dic_donasi_bulan)
+pil_donbul = st.selectbox('pilih bulan',bulan_unik)
+if pil_donbul==bulan_unik[0]:
+    st.write(don_bul[0:1])
+if pil_donbul==bulan_unik[0]:
+    st.write(don_bul[1:2])
+if pil_donbul==bulan_unik[0]:
+    st.write(don_bul[2:3])
+if pil_donbul==bulan_unik[0]:
+    st.write(don_bul[3:4])
+if pil_donbul==bulan_unik[0]:
+    st.write(don_bul[4:5])
