13ce4ce917743e6565c3f95c2061ea4363fdfe7a
feriyantosrb <86914580+feriyantosrb@users.noreply.github.com>
2021-12-20 18:04:55 +0700

    Add files via upload

--- rekap.py
+++ rekap.py
@@ -22,155 +22,105 @@ df = ch_.dataFrame
 
 st.set_page_config(layout="wide")
 
-st.title('DONASI IAAF')
-st.header('Okeee')
-st.dataframe(df)
+st.title('Program Ikatan Alumni Al-Uswah Foundation')
+st.header('Rekapitulasi Update Donasi')
 
-'''
 #Konfigurasi Pengaturan
 config = st.sidebar.radio("Pilih bagian yang ingin Anda ketahui", ('Deskripsi', 'Rekapitulasi', 'Kontak Kami'))
-#if......
-'''
+if config == 'Deskripsi':
+    st.write('ini adalah bagian deskripsi')
+if config == 'Rekapitulasi':
+    st.write('ini adalah bagian rekapitulasi')
+else:
+    st.write('ini adalah bagian kontak kami')
 
-left_col, mid_col, right_col = st.columns(3)
+left_col, right_col = st.columns(2)
 
 #upper left col
-#dict jenis unik, total donasi
+#start jenis unik, total donasi
 Jenis_unik = list(df['Jenis'].unique())
-pilih = left_col.selectbox('masukkan pilihan',Jenis_unik)
 total_donasi = []
 for Jenis in Jenis_unik:
-    Nominal = df[df['Jenis']==Jenis]['Nominal'].astype(int)
+    Nominal = df[df['Jenis']==Jenis]['Nominal'].str.replace(',','').astype(int)
     total_donasi.append(Nominal.sum())
-dic_jen_tot ={'Jenis Donasi':Jenis_unik,'total donasi':total_donasi}
-data = pd.DataFrame(dic_jen_tot)
-if pilih == Jenis_unik [0]:
-    st.write(data[0:1])
-else :
-    st.write(data[1:])
+dic_jen_tot ={'Jenis Donatur':Jenis_unik,'Total Donasi':total_donasi}
+jenis_total = pd.DataFrame(dic_jen_tot)
+pilih_jenis = right_col.selectbox('Pilih Jenis Donatur',Jenis_unik)
+def a_ ():
+    a = Jenis_unik.index(pilih_jenis)
+    return a
+def b_():
+    frame_jenis = jenis_total [a_():a_()+1]
+    return frame_jenis
+right_col.write(b_())
+#finish jenis, total donasi
 
-
-#upper middle col
-#membuat nama unik
+#start Nama unik, Jumlah donasi
 nama_unik=list(df['Nama'].unique()) #nama unik
 jumlah_nominal = []
 for nama in nama_unik:
-    sub_nom = df[df['Nama']==nama]['Nominal'].astype(int)
+    sub_nom = df[df['Nama']==nama]['Nominal'].str.replace(',','').astype(int)
     jumlah_nominal.append(sub_nom.sum())
-dic_nama_jumnom={'Nama':nama_unik,'Jumlah Donasi':jumlah_nominal}
+dic_nama_jumnom={'Nama Donatur':nama_unik,'Jumlah Donasi':jumlah_nominal}
 nama_jumnom = pd.DataFrame(dic_nama_jumnom)
-#print(nama_jumnom)
-choose = st.selectbox('pilih nama donatur',nama_unik) # nama_unik[0],....
-def f_ ():
-    a = nama_unik.index(choose)
-    return a
-def g_():
-    datframe = nama_jumnom[f_():f_()+1]
-    return datframe
-left_col.write(g_())
-'''
-if choose == nama_unik[0]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[1]:
-    st.write(nama_jumnom[1:2])
-if choose == nama_unik[2]:
-    st.write(nama_jumnom[2:3])
-if choose == nama_unik[3]:
-    st.write(nama_jumnom[3:4])
-if choose == nama_unik[4]:
-    st.write(nama_jumnom[4:5])
-if choose == nama_unik[5]:
-    st.write(nama_jumnom[5:6])
-if choose == nama_unik[6]:
-    st.write(nama_jumnom[6:7])
-if choose == nama_unik[7]:
-    st.write(nama_jumnom[7:8])
-if choose == nama_unik[8]:
-    st.write(nama_jumnom[8:9])
-if choose == nama_unik[9]:
-    st.write(nama_jumnom[9:10])
-if choose == nama_unik[10]:
-    st.write(nama_jumnom[10:11])
-if choose == nama_unik[11]:
-    st.write(nama_jumnom[11:12])
-if choose == nama_unik[12]:
-    st.write(nama_jumnom[12:13])
-if choose == nama_unik[13]:
-    st.write(nama_jumnom[13:14])
-if choose == nama_unik[14]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[15]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[16]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[17]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[18]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[19]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[20]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[21]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[22]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[23]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[24]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[25]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[26]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[27]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[28]:
-    st.write(nama_jumnom[0:1])
-if choose == nama_unik[29]:
-    st.write(nama_jumnom[0:1])
-'''
+tulis_nama = []
+for i, nama in enumerate(nama_unik):
+    tulis_nama.append(f"{str(i+1)}. {nama}\n")
+tulis_nama = ' '.join(map(str, tulis_nama))
 
-#lower left col
-#tujuan unik
+left_col.subheader('Daftar Nama Donatur')
+left_col.dataframe(tulis_nama)
+
+right_col.subheader('Nominal Donasi Berdasarkan Donatur')
+choose = right_col.selectbox('Pilih Nama Donatur',nama_unik) # nama_unik[0],....
+def c_ ():
+    c = nama_unik.index(choose)
+    return c
+def d_():
+    d = nama_jumnom[c_():c_()+1]
+    return d
+right_col.write(d_())
+#finish nama, jumlah
+
+#start tujuan unik, Nominal
 tujuan_unik = list(df['Tujuan'].unique())
 donasi_tujuan = []
 for tujuan in tujuan_unik:
-    sub_tujuan = df[df['Tujuan']==tujuan]['Nominal'].astype(int)
+    sub_tujuan = df[df['Tujuan']==tujuan]['Nominal'].str.replace(',','').astype(int)
     donasi_tujuan.append(sub_tujuan.sum())
-dic_donasi_tujuan={'Tujuan':tujuan_unik,'Jumlah donasi':donasi_tujuan}
+dic_donasi_tujuan={'Alamat Donasi Tujuan':tujuan_unik,'Nominal':donasi_tujuan}
 don_tu = pd.DataFrame(dic_donasi_tujuan)
-pil_dontu = st.selectbox('pilih alamat donasi tujuan',tujuan_unik)
-if pil_dontu == tujuan_unik[0]:
-    st.write(don_tu[0:1])
-if pil_dontu == tujuan_unik[1]:
-    st.write(don_tu[1:2])
-if pil_dontu == tujuan_unik[2]:
-    st.write(don_tu[2:3])
-if pil_dontu == tujuan_unik[3]:
-    st.write(don_tu[3:4])
-if pil_dontu == tujuan_unik[4]:
-    st.write(don_tu[4:5])
-if pil_dontu == tujuan_unik[5]:
-    st.write(don_tu[5:6])
+right_col.write()
+right_col.subheader('Nominal Donasi Berdasarkan Alamat Donasi Tujuan')
+pil_dontu = right_col.selectbox('Pilih Alamat Donasi Tujuan',tujuan_unik)
+def e_ ():
+    e = nama_unik.index(choose)
+    return e
+def f_():
+    f = nama_jumnom[e_():e_()+1]
+    return f
+right_col.write(f_())
+#finish tujuan, nominal
 
-#bawah
-#dic bulan unik, jumlah donasi (Donatur Tetap)
+#start bulan unik, jumlah donasi (Donatur Tetap)
 bulan_unik = list(df['Bulan'].unique())
 donasi_bulan = []
 for bulan in bulan_unik:
-    sub_bulan = df[df['Bulan']==bulan]['Nominal'].astype(int)
+    sub_bulan = df[df['Bulan']==bulan]['Nominal'].str.replace(',','').astype(int)
     donasi_bulan.append(sub_bulan.sum())
-dic_donasi_bulan={'Bulan':bulan_unik,'Jumlah Donasi':donasi_bulan}
+dic_donasi_bulan={'Bulan':bulan_unik,'Nominal':donasi_bulan}
 don_bul = pd.DataFrame(dic_donasi_bulan)
-pil_donbul = st.selectbox('pilih bulan',bulan_unik)
-if pil_donbul==bulan_unik[0]:
-    st.write(don_bul[0:1])
-if pil_donbul==bulan_unik[1]:
-    st.write(don_bul[1:2])
-if pil_donbul==bulan_unik[2]:
-    st.write(don_bul[2:3])
-if pil_donbul==bulan_unik[3]:
-    st.write(don_bul[3:4])
-if pil_donbul==bulan_unik[4]:
-    st.write(don_bul[4:5])
+right_col.write()
+right_col.subheader('Nominal Donasi Berdasarkan Bulan')
+pil_donbul = right_col.selectbox('Pilih Bulan',bulan_unik)
+def g_ ():
+    g = nama_unik.index(choose)
+    return g
+def h_():
+    h = nama_jumnom[g_():g_()+1]
+    return h
+right_col.write(h_())
+
+st.write()
+st.header('Summary')
+st.dataframe(df)
